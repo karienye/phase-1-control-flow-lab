6d59e023db7cb2b168688ae51b02fc7891fae0fc
leah karienye <karienyeleah@gmail.com>
2022-06-07 02:52:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(message){
   // Write your code here!
+  let distance = 300;
+  if (distance >=2000 ) {
+    message = 'This one is on me!';
+  }
+  
+  else if (distance > 400)
+  message = 'I will gladly take your thirty bucks.';
+
 }
 
+
 function ternaryCheckCity(){
-  // Write your code here!
+  const city = NYC;
+
+let message;
+
+city === NYC? (message = "Ok, sounds good.") : (message = "No go.");
 }
 
 function switchOnCharmFromTip(){
